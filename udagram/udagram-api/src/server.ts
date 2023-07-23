3ef389c9b6ff02c74360b3760ca9c1c9803b2c4e
nhnam <37695601+hoai97nam@users.noreply.github.com>
2023-07-23 20:35:31 +0700

    fix

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -30,6 +30,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   app.use(bodyParser.json());
 
   app.use(cors());
+  app.options('*', cors())
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
   // app.use(cors({
