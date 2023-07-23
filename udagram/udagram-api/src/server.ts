8a4a4ae867cb8ac0d4d213eb61752a9912902d71
nhnam <37695601+hoai97nam@users.noreply.github.com>
2023-07-23 22:28:01 +0700

    fix cors

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -29,21 +29,24 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
+  const corsOptions = {
+    origin: ['http://udagram-api-dev22.us-east-1.elasticbeanstalk.com', 'https://subdomain.example.com'],
+  };
+  app.use(cors(corsOptions));
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
-  app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
-    ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
-  }));
+  // app.use(cors({
+  //   "allowedHeaders": [
+  //     'Origin', 'X-Requested-With',
+  //     'Content-Type', 'Accept',
+  //     'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
+  //     'Access-Control-Allow-Headers',
+  //     'Access-Control-Allow-Methods'
+  //   ],
+  //   "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  //   "preflightContinue": true,
+  //   "origin": '*',
+  // }));
 
   app.use("/api/v0/", IndexRouter);
 
